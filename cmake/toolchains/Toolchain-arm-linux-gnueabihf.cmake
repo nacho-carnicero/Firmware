cc60335dd27e6490fae97e89d079e246451719ce
nacho-carnicero <i.carnicerotresca@gmail.com>
2016-04-29 10:36:49 +0200

    Modify the cmake for cross compiling the firmware for RPi
    Replaces the cmake file in cmake/toolchains/Toolchain-arm-linux-gnueabihf.cmake
    so that it sets correctly the compiler for RPi and avoids looking for
    the HEXAGON_SDK_ROOT that is needed for compiling the firmware for
    Snapdragon but not for Rpi

--- cmake/toolchains/Toolchain-arm-linux-gnueabihf.cmake
+++ cmake/toolchains/Toolchain-arm-linux-gnueabihf.cmake
@@ -1,93 +1,44 @@
-# defines:
+############################################################################
+# Cross-compilation Toolchain File (CMAKE_TOOLCHAIN_FILE)
+# for Raspberry Pi 2
+#     requires a proper toolchain setup:
+#     https://github.com/pixhawk/rpi_toolchain
 #
-# NM
-# OBJCOPY
-# LD
-# CXX_COMPILER
-# C_COMPILER
-# CMAKE_SYSTEM_NAME
-# CMAKE_SYSTEM_VERSION
-# GENROMFS
-# LINKER_FLAGS
-# CMAKE_EXE_LINKER_FLAGS
-# CMAKE_FIND_ROOT_PATH
-# CMAKE_FIND_ROOT_PATH_MODE_PROGRAM
-# CMAKE_FIND_ROOT_PATH_MODE_LIBRARY
-# CMAKE_FIND_ROOT_PATH_MODE_INCLUDE
-
+# Author: Bo Liu (bo-rc@acm.org)
+#
+############################################################################
 include(CMakeForceCompiler)
 
-if ("$ENV{HEXAGON_SDK_ROOT}" STREQUAL "")
-        message(FATAL_ERROR "HEXAGON_SDK_ROOT not set")
-else()
-        set(HEXAGON_SDK_ROOT $ENV{HEXAGON_SDK_ROOT})
-endif()
+add_definitions(
+	-D__RPI2
+	)
 
-if ("$ENV{HEXAGON_ARM_SYSROOT}" STREQUAL "")
-        message(FATAL_ERROR "HEXAGON_ARM_SYSROOT not set")
-else()
-        set(HEXAGON_TOOLS_ROOT $ENV{HEXAGON_ARM_SYSROOT})
+######### test DriverFramework for rpi2 ###
+# used for debug
+#add_definitions(-DDF_DEBUG)
+
+if ("${RPI_TOOLCHAIN_DIR}" STREQUAL "")
+	set(RPI_TOOLCHAIN_DIR /opt/rpi_toolchain)
 endif()
 
 # this one is important
-set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_NAME Linux)
 
-#this one not so much
+# this one not so much
 set(CMAKE_SYSTEM_VERSION 1)
 
-# specify the cross compiler
-find_program(C_COMPILER arm-linux-gnueabihf-gcc
-	PATHS ${HEXAGON_SDK_ROOT}/gcc-linaro-arm-linux-gnueabihf-4.8-2013.08_linux/bin
-	NO_DEFAULT_PATH
-	)
-
-if(NOT C_COMPILER)
-	message(FATAL_ERROR "could not find arm-linux-gnueabihf-gcc compiler")
-endif()
-cmake_force_c_compiler(${C_COMPILER} GNU)
-
-find_program(CXX_COMPILER arm-linux-gnueabihf-g++
-	PATHS ${HEXAGON_SDK_ROOT}/gcc-linaro-arm-linux-gnueabihf-4.8-2013.08_linux/bin
-	NO_DEFAULT_PATH
-	)
-
-if(NOT CXX_COMPILER)
-	message(FATAL_ERROR "could not find arm-linux-gnueabihf-g++ compiler")
-endif()
-cmake_force_cxx_compiler(${CXX_COMPILER} GNU)
-
-# compiler tools
-foreach(tool objcopy nm ld)
-	string(TOUPPER ${tool} TOOL)
-	find_program(${TOOL} arm-linux-gnueabihf-${tool}
-		PATHS ${HEXAGON_SDK_ROOT}/gcc-linaro-arm-linux-gnueabihf-4.8-2013.08_linux/bin
-		NO_DEFAULT_PATH
-		)
-	if(NOT ${TOOL})
-		message(FATAL_ERROR "could not find arm-linux-gnueabihf-${tool}")
-	endif()
-endforeach()
-
-# os tools
-foreach(tool echo patch grep rm mkdir nm genromfs cp touch make unzip)
-	string(TOUPPER ${tool} TOOL)
-	find_program(${TOOL} ${tool})
-	if(NOT ${TOOL})
-		message(FATAL_ERROR "could not find ${TOOL}")
-	endif()
-endforeach()
-
-set(C_FLAGS "--sysroot=${HEXAGON_ARM_SYSROOT}")
+# set cross compilers
+set(CMAKE_C_COMPILER "${RPI_TOOLCHAIN_DIR}/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf-gcc")
+set(CMAKE_CXX_COMPILER "${RPI_TOOLCHAIN_DIR}/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf-g++")
+set(CMAKE_C_FLAGS "")
 set(LINKER_FLAGS "-Wl,-gc-sections")
 set(CMAKE_EXE_LINKER_FLAGS ${LINKER_FLAGS})
-set(CMAKE_C_FLAGS ${C_FLAGS})
-set(CMAKE_CXX_LINKER_FLAGS ${C_FLAGS})
 
-# where is the target environment
-set(CMAKE_FIND_ROOT_PATH  get_file_component(${C_COMPILER} PATH))
+CMAKE_FORCE_C_COMPILER(arm-linux-gnueabihf-gcc GNU)
+CMAKE_FORCE_CXX_COMPILER(arm-linux-gnueabihf-g++ GNU)
 
 # search for programs in the build host directories
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 # for libraries and headers in the target directories
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
